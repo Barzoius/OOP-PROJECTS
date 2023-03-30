2f3a29781934b4fe46896827845a479b7c0887ba
Barzoius <95474661+Barzoius@users.noreply.github.com>
2023-03-30 09:02:03 -1100

    Update OOP.cpp

--- OOP.cpp
+++ OOP.cpp
@@ -6,7 +6,11 @@
 temmplate<int N>
 using Serial = std::integral_const<bool, (N > 3)>;
 
-class SerialKiller {};
+class SerialKiller {
+private:
+  int IQ;
+  Serial count;
+};
 
 class ThrillSeeker : public virtual SerialKiller {};
 
