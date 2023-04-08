0898ea1b011389a24779a80013b3e68db156911b
Barzoius <95474661+Barzoius@users.noreply.github.com>
2023-04-07 22:42:03 -1100

    Update OOP.cpp

--- OOP.cpp
+++ OOP.cpp
@@ -31,6 +31,21 @@ public:
           std::cout <<  this -> Name<<" is not a Serial Killer.";
    }
   
+  SerialKiller(char* name, int killcount, float iq ) :killcount(killcount), IQ(iq) {
+
+      Name = new char[strlen(name) + 1];
+      strcpy(Name, name);
+
+      if(getKillCount() >= 3)
+          if(getIQ() >= 150)
+              std::cout << getName() <<" is a SerialKiller with an IQ of at least 150 making him a possible fit for the  MissionOriented or Visionary type.";
+          else
+              {std::cout<< getName() <<" is a SerialKiller with an IQ under 150 so probably a ThrillSeeker or a ControlSeeker type.";}
+      else
+          std::cout<< getName() << " is not a Serial Killer.";
+
+  }
+  
       SerialKiller(const char* n)
     {
         Name = new char[strlen(n) + 1];
