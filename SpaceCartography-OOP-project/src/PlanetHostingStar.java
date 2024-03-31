896a436650572af0237a0d14dde1981e1ff6d50d
Barzoius <mecca.rares@gmail.com>
2024-03-31 16:36:52 +0300

    added Spacecartography

---
+++ SpaceCartography-OOP-project/src/PlanetHostingStar.java
@@ -0,0 +1,8 @@
+public class PlanetHostingStar extends Star {
+
+//    public PlanetHostingStar(String name, double diameter, double gravPull)
+//    {
+//        super(name, diameter, gravPull);
+//
+//    }
+}
